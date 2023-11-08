82c73393442ac3ec1e3207ca57dab97ff4823486
ho-bolt <gbolt@naver.com>
2023-11-08 11:45:55 +0900

    Revert "✨ Feat : restaruants 모듈, Resolver 추가"
    This reverts commit 381abfa149de40ed099240d7008d8c4f90101312.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,10 @@
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
-import { RestaurantsModule } from './restaurants/restaurants.module';
-// app.module는 main.ts로 import 되는 유일한 모듈이다.
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: true,
-    }),
-    RestaurantsModule,
-  ],
-  controllers: [],
-  providers: [],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/restaurants/restaurants.resolver.ts
+++
@@ -1,9 +0,0 @@
-import { Resolver, Query } from '@nestjs/graphql';
-
-@Resolver()
-export class RestaurantResolver {
-  @Query((returns) => Boolean)
-  isPizzGood(): Boolean {
-    return true;
-  }
-}
