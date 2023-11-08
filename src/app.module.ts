381abfa149de40ed099240d7008d8c4f90101312
ho-bolt <gbolt@naver.com>
2023-11-08 11:45:45 +0900

    ✨ Feat : restaruants 모듈, Resolver 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { RestaurantsModule } from './restaurants/restaurants.module';
+// app.module는 main.ts로 import 되는 유일한 모듈이다.
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    RestaurantsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
