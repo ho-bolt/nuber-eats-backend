86e46792e10d77f03ddad8792df6715dc265937b
ho-bolt <gbolt@naver.com>
2023-11-10 10:24:01 +0900

    🚧 Chore : restaurant 엔티티 추가
    - restaurant 에 grapql의 @objectType외에도 @Entity 추가
    - TypeOrm으로 postgresql 연결
    - restaurant 테이블 작성
    
    close

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,10 @@ import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { GraphQLModule } from '@nestjs/graphql';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { Restaurant } from './restaurants/entities/restaurant.entity';
+// javascript 패키지를 import from 으로 사용하는 방법
 
 @Module({
   imports: [
@@ -13,15 +17,29 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoSchemaFile: true,
     }),
     RestaurantsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+      }),
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5433,
-      username: 'postgres',
-      password: 'krsoft0719',
-      database: 'nuber_eats',
-      synchronize: true,
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      synchronize: process.env.NODE_ENV !== 'prod', // typeOrm이 계속 entity를 동기화 시킴 따라서 개발할 때만 동기화 시키고 아닐때는 동기화 방지
       logging: true,
+      entities: [Restaurant],
     }),
   ],
   controllers: [AppController],
--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,16 +1,40 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Restaurant {
   // 여기 type 는 큰 의미 없다 편한대로 작성해도 된다.
+
+  @PrimaryGeneratedColumn()
+  @Field((type) => Number)
+  id: number;
+
   @Field((type) => String)
+  @Column()
   name: string;
+
   @Field((type) => Boolean)
+  @Column()
   isVegan?: boolean;
 
   @Field((type) => String)
+  @Column()
   address: string;
 
   @Field((type) => String)
+  @Column()
   ownerName: string;
+
+  @Field((type) => String)
+  @Column()
+  categoryName: string;
 }
+
+// Graphql 에서 @ObjectType
+// -> 자동으로 스키마를 빌드하기 위해 사용하는 Graphql 데코레이터
+
+// TypeOrm
+// -> @Entity 는 TypeOrm이 db에 이걸 저장하게 해준다.
+
+// 둘다 사용해서 grqphql 스키마와 db에 저장되는 데이터 형식을 만들 수 있다.
