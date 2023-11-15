653923511164c1000a7e9a149156452d1be4fa2b
ho-bolt <gbolt@naver.com>
2023-11-15 13:47:20 +0900

    ✨ Feat : jwt 모듈 추가
    - jwt를 따로 관리할 수 있도록 모듈 추가
    - jwt 토큰 생성 기능 추가
    - privateKey 관리를 위해 JwtModuleOptions 분리

---
+++ src/jwt/jwt.service.ts
@@ -0,0 +1,22 @@
+import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
+import { Inject } from '@nestjs/common/decorators';
+import { CONFIG_OPTIONS } from './jwt.constants';
+import { JwtModuleOptions } from './interfaces/jwt.interfaces';
+import * as jwt from 'jsonwebtoken';
+
+@Injectable()
+export class JwtService {
+  constructor(
+    @Inject(CONFIG_OPTIONS) private readonly options: JwtModuleOptions,
+  ) // private readonly configService: ConfigService,
+  {}
+
+  sign(payload: object): string {
+    // jwt module 사용 방법
+    return jwt.sign(payload, this.options.privateKey);
+
+    // global configService 사용 방법
+    //return this.configService.get('PRIVATE_KEY');
+  }
+}
