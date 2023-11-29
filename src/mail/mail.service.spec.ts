55d6b6333e8567086c0809d6365a01ecc8c01e58
ho-bolt <gbolt@naver.com>
2023-11-29 09:05:02 +0900

    🧪 Test : jwt token unit test
    - jwt sign unit test
    - jwt verify unit test
    
    close

---
+++ src/mail/mail.service.spec.ts
@@ -0,0 +1,80 @@
+import { Test } from '@nestjs/testing';
+import { MailService } from './mail.service';
+import { CONFIG_OPTIONS } from 'src/common/common.constants';
+import * as FormData from 'form-data';
+import got from 'got';
+
+jest.mock('got');
+jest.mock('form-data');
+
+const TEST_DOMAIN = 'test-domain';
+describe('MailService', () => {
+  let service: MailService;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        MailService,
+        {
+          provide: CONFIG_OPTIONS,
+          useValue: {
+            apiKey: 'test-apikey',
+            domain: TEST_DOMAIN,
+            fromEmail: 'test-fromEmail',
+          },
+        },
+      ],
+    }).compile();
+    service = module.get<MailService>(MailService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('sendVerificationEmail', () => {
+    it('should call sendEmail', () => {
+      const sendVerificationEmailArgs = {
+        email: 'email',
+        code: 'code',
+      };
+      jest.spyOn(service, 'sendEmail').mockImplementation(async () => true);
+      service.sendVerificationEmail(
+        sendVerificationEmailArgs.email,
+        sendVerificationEmailArgs.code,
+      );
+      expect(service.sendEmail).toHaveBeenCalledTimes(1);
+      expect(service.sendEmail).toHaveBeenCalledWith(
+        'Verify Your Email',
+        'verify-email',
+        [
+          { key: 'code', value: sendVerificationEmailArgs.code },
+          { key: 'name', value: sendVerificationEmailArgs.email },
+        ],
+      );
+    });
+  });
+
+  describe('sendEmail', () => {
+    it('sends email', async () => {
+      const ok = await service.sendEmail('', '', []);
+      // FormData는 sendEmail에서 new class로 생성하기 때문에 prototype으로 가져와 append 함수를 spy한다.
+      //class를 spy하기 위해서는 prototype을 사용하면 된다.
+      const formSpy = jest.spyOn(FormData.prototype, 'append');
+      expect(formSpy).toHaveBeenCalled();
+      expect(got.post).toHaveBeenCalledTimes(1);
+      expect(got.post).toHaveBeenCalledWith(
+        `https://api.mailgun.net/v3/${TEST_DOMAIN}/messages`,
+        expect.any(Object),
+      );
+      expect(ok).toEqual(true);
+    });
+    it('fails on error', async () => {
+      jest.spyOn(got, 'post').mockImplementation(() => {
+        throw new Error();
+      });
+      const ok = await service.sendEmail('', '', []);
+      expect(ok).toEqual(false);
+    });
+  });
+});
